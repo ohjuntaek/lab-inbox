18f3505583668397245155ed4d18d9d0afe11dd3
ohjuntaek <201211734@pukyong.ac.kr>
2022-09-24 17:33:34 +0900

    merge into main from lab-multi-module

---
+++ inbox-domain/src/main/java/lab/juntaek/inbox/ptwas/inbox/domain/Main.java
@@ -0,0 +1,8 @@
+package lab.juntaek.inbox.ptwas.inbox.domain;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
---
+++ inbox-pt-was/src/main/java/lab/juntaek/inbox/ptwas/InboxPtWasApplication.java
@@ -0,0 +1,13 @@
+package lab.juntaek.inbox.ptwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxPtWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxPtWasApplication.class, args);
+	}
+
+}
